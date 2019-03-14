080b772e78f20cc97c39687a18f86d78712b02c1
Heeeun <lheeeun@amazon.com>
2019-03-14 20:56:55 +0100

    exception second class

--- src/org/redi/Main.java
+++ src/org/redi/Main.java
@@ -1,5 +1,7 @@
 package org.redi;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.List;
@@ -9,8 +11,11 @@ public class Main {
 
     public static void main(String[] args) {
 
+        System.out.println("We've got the number : " + getUserInputNumber());
+
         //System.out.println("We got the number: " + getNumber());
 
+        /*
         Scanner scanner = new Scanner(System.in);
 
         List<String> stringList = new ArrayList<String>();
@@ -34,6 +39,7 @@ public class Main {
         } finally {
             System.out.println("Thank you for entering inputs");
         }
+        */
 
         /*
          catch (Exception e) {
@@ -47,6 +53,27 @@ public class Main {
         //stringList.get(2);
     }
 
+    static int getUserInputNumber() {
+
+        while (true) {
+            try {
+                Scanner scanner = new Scanner(System.in);
+                int number = scanner.nextInt();
+                if (number > 2) {
+                    throw new RediAppException("The user input should be smaller than 3");
+                }
+
+                return number;
+
+            } catch (InputMismatchException e) {
+                System.out.println("You should enter a number.");
+                //throw new FileNotFoundException("A number is expected.", e);
+            } catch (RediAppException e) {
+                System.out.println("You should enter a number smaller than 3.");
+            }
+        }
+    }
+
     static int getNumber() {
         while (true) {
             try {
@@ -57,7 +84,7 @@ public class Main {
                 return number;
             } catch (InputMismatchException e) {
                 System.out.println("Please enter a number");
-            } catch (Exception e) {
+            } catch (RuntimeException e) {
                 System.out.println("Something happened");
             }
         }
---
+++ src/org/redi/RediAppException.java
@@ -0,0 +1,7 @@
+package org.redi;
+
+public class RediAppException extends Exception {
+    public RediAppException(String message) {
+        super(message);
+    }
+}
